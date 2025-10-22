6acb9441b2bcc10a4f4253ffb6fe06eb5fb02feb
jylli@office <li_jiyanglin@163.com>
2025-10-22 14:59:50 +0800

    减复数静态分量+预测后融合

--- TimeEmb-main/models/TimeEmb.py
+++ TimeEmb-main/models/TimeEmb.py
@@ -21,10 +21,24 @@ class Model(nn.Module):
                 nn.ReLU(),
                 nn.Linear(self.d_model, self.pred_len)
         ]
+        layers_hour = [
+                nn.Linear(self.seq_len, self.d_model),
+                nn.ReLU(),
+                nn.Linear(self.d_model, self.pred_len)
+        ]
+        layers_day = [
+                nn.Linear(self.seq_len, self.d_model),
+                nn.ReLU(),
+                nn.Linear(self.d_model, self.pred_len)
+        ]
         self.model = nn.Sequential(*layers)
+        self.model_hour = nn.Sequential(*layers_hour)
+        self.model_day = nn.Sequential(*layers_day)
 
-        self.emb_hour = nn.Parameter(torch.zeros(self.emb_len_hour, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
-        self.emb_day = nn.Parameter(torch.zeros(self.emb_len_day, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
+        self.emb_hour_re = nn.Parameter(torch.zeros(self.emb_len_hour, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
+        self.emb_hour_im = nn.Parameter(torch.zeros(self.emb_len_hour, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
+        self.emb_day_re = nn.Parameter(torch.zeros(self.emb_len_day, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
+        self.emb_day_im = nn.Parameter(torch.zeros(self.emb_len_day, self.enc_in, self.seq_len // 2 + 1), requires_grad=True)
         self.w = nn.Parameter(self.scale * torch.randn(1, self.seq_len))
 
     def forward(self, x, hour_index, day_index = None):
@@ -38,30 +52,44 @@ class Model(nn.Module):
         x = x.permute(0, 2, 1)
         x = torch.fft.rfft(x, dim=2, norm='ortho')
         w = torch.fft.rfft(self.w, dim=1, norm='ortho')
-        x_freq_real = x.real
-        x_freq_imag = x.imag
+        # x_freq_real = x.real
+        # x_freq_imag = x.imag
 
         if self.use_hour_index:
-            emb_hour = self.emb_hour[hour_index % self.emb_len_hour]
-            x_freq_real = x_freq_real - emb_hour
+            emb_hour_re = self.emb_hour_re[(hour_index % self.emb_len_hour).long()]
+            emb_hour_im = self.emb_hour_im[(hour_index % self.emb_len_hour).long()]
+            emb_hour = torch.complex(emb_hour_re, emb_hour_im)
+            x = x - emb_hour
 
         if self.use_day_index:
-            emb_day = self.emb_day[day_index % self.emb_len_day]
-            x_freq_real = x_freq_real - emb_day
+            emb_day_re = self.emb_day_re[(day_index % self.emb_len_day).long()]
+            emb_day_im = self.emb_day_im[(day_index % self.emb_len_day).long()]
+            emb_day = torch.complex(emb_day_re, emb_day_im)
+            x = x - emb_day
 
-        x_freq_minus_emb = torch.complex(x_freq_real, x_freq_imag)
-        y = x_freq_minus_emb * w
-        y_real = y.real
-        y_freq_imag = y.imag
+        # x_freq_minus_emb = torch.complex(x_freq_real, x_freq_imag)
+        # y = x * w
+        # y_real = y.real
+        # y_freq_imag = y.imag
 
-        if self.use_day_index:
-            y_real = y_real + emb_day
-        if self.use_hour_index:
-            y_real = y_real + emb_hour
+        # if self.use_day_index:
+        #     y_real = y_real + emb_day
+        # if self.use_hour_index:
+        #     y_real = y_real + emb_hour
 
-        y_freq = torch.complex(y_real, y_freq_imag)
+        y_freq = x * w
         y = torch.fft.irfft(y_freq, n=self.seq_len, dim=2, norm="ortho")
         y = self.model(y).permute(0, 2, 1)
+
+        if self.use_hour_index:
+            y_hour = torch.fft.irfft(emb_hour, n=self.seq_len, dim=2, norm="ortho")
+            y_hour = self.model_hour(y_hour).permute(0, 2, 1)
+            y = y + y_hour
+
+        if self.use_day_index:
+            y_day = torch.fft.irfft(emb_day, n=self.seq_len, dim=2, norm="ortho")
+            y_day = self.model_day(y_day).permute(0, 2, 1)
+            y = y + y_day
 
         if self.use_revin:
             y = y * torch.sqrt(seq_var) + seq_mean
